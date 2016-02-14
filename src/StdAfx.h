52ca7c3874c7907168ca732d972290fbc5a5da53
Chris Barrett <chris@codesaru.com>
2016-02-13 18:02:56 -0800

    Remove StdAfx files.

--- src/StdAfx.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cstdio>
-
-#include <csaru-core-cpp/csaru-core-cpp.h>
-#include <csaru-container-cpp/csaru-container-cpp.h>
-#include <csaru-json-cpp/csaru-json-cpp.h>
-#include <csaru-serialization-cpp/csaru-serialization-cpp.h>
-
